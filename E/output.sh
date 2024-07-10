25bf914603a1d413a168553d72469c57861bcc06
noahburren <noah.burren@bluewin.ch>
2024-07-10 08:46:54 +0200

    e und f

---
+++ E/output.sh
@@ -0,0 +1,15 @@
+noah@LaptopNoah:~$ ./AufgabeE.sh
+grep: *.data: No such file or directory
+grep: lookbehind assertion is not fixed length
+grep: *.data: No such file or directory
+grep: *.data: No such file or directory
+Connected to haraldmueller.ch.
+220 ProFTPD Server (ProFTPD) [82.195.251.67]
+331 Password required for schoolerinvoices
+230 User schoolerinvoices logged in
+550 /out/AP22bKos: No such file or directory
+local: qr_rechnungen.zip remote: qr_rechnungen.zip
+ftp: Can't open `qr_rechnungen.zip': No such file or directory
+221 Goodbye.
+QR-Rechnungen erfolgreich hochgeladen.
+noah@LaptopNoah:~$
